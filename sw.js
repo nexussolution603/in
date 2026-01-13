16b57b88ff055efec6d03520d37fe1ac157bc3b0
NEXSUS SOLUTION <nexussolution603@gmail.com>
2026-01-13 21:18:10 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Service Worker for Nexus Solution App
+const CACHE_NAME = 'nexus-solution-v1.0';
+
+// Files to cache for offline use
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/sw.js'
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
